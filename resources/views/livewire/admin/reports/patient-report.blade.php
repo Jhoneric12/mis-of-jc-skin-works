768cb4c3c2aae50512446936d4c4eb486ff944d0
unknown <jhonerix23@gmail.com>
2024-03-31 23:26:00 +0800

    removed actions in patient report

--- resources/views/livewire/admin/reports/patient-report.blade.php
+++ resources/views/livewire/admin/reports/patient-report.blade.php
@@ -52,9 +52,9 @@
                     {{-- <th scope="col" class="px-6 py-6">
                         Status
                     </th> --}}
-                    <th scope="col" class="px-6 py-6">
+                    {{-- <th scope="col" class="px-6 py-6">
                         Action
-                    </th>
+                    </th> --}}
                 </tr>
             </thead>
             <tbody>
@@ -88,7 +88,7 @@
                             {{ $patient->account_status ? 'Active' : 'Inactive'}}
                         </span>
                     </td> --}}
-                    <td class="px-6 py-6 flex gap-2 items-center">
+                    {{-- <td class="px-6 py-6 flex gap-2 items-center">
                         <a href="{{route('view-profile', ['patient_id' => $patient->id])}}">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-400">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
@@ -100,7 +100,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                             </svg>
                         </a>
-                    </td>
+                    </td> --}}
                 </tr>
                 @empty
                     <tr class="w-full">

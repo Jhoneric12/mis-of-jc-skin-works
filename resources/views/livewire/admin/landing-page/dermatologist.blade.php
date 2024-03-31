114250566e21098cf141707b1b4e7e1236372275
unknown <jhonerix23@gmail.com>
2024-03-31 23:09:19 +0800

    modified dermatologsit photo height

--- resources/views/livewire/admin/landing-page/dermatologist.blade.php
+++ resources/views/livewire/admin/landing-page/dermatologist.blade.php
@@ -7,7 +7,7 @@
         </div>
     </div>
     <div class="flex justify-center">
-        <img src="{{asset('storage/' . $image)}}" alt="" class="w-[20rem] h-[10rem] md:w-[20rem] md:h-[20rem]">
+        <img src="{{asset('storage/' . $image)}}" alt="" class="w-[20rem] h-[15rem] md:w-[20rem] md:h-[20rem]">
     </div>
     <div class="bg-white shadow-lg rounded-lg px-20 py-4 mb-10 mt-4">
         <h1 class="text-primary-green font-bold text-sm">Dr. {{$dermaName}}</h1>

71d852897ee958423d5746fc29e98ee7d8845046
unknown <jhonerix23@gmail.com>
2024-04-09 23:05:41 +0800

    added w-full in authentication card

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen w-full flex flex-col justify-center items-center lg:flex lg:flex-row lg:justify-around pt-6 py-10 sm:pt-0 ">
+<div class="min-h-screen w-full flex flex-col justify-center items-center md:flex md:flex-row md:justify-around pt-6 py-10 sm:pt-0 ">
     <div>
         {{ $logo }}
     </div>
 
-    <div {{$attributes->merge(['class' => "sm:max-w-3xl mt-6 px-6 py-10 bg-white shadow-md overflow-hidden sm:rounded-lg"])}}>
+    <div {{$attributes->merge(['class' => "w-full sm:max-w-3xl mt-6 px-6 py-10 bg-white shadow-md overflow-hidden sm:rounded-lg"])}}>
         {{ $slot }}
     </div>
 </div>

b16ff1b4c0a99a254232b192c3163a86fef0bf63
unknown <jhonerix23@gmail.com>
2024-04-09 23:55:23 +0800

    modified login and signup again

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen w-full flex flex-col justify-center items-center md:grid md:grid-cols-2 md:place-items-center py-16 ">
+<div class="min-h-screen w-full flex flex-col justify-center items-center md:flex md:flex-row md:justify-around py-10 ">
     <div>
         {{ $logo }}
     </div>
 
-    <div {{$attributes->merge(['class' => "w-full sm:max-w-2xl mt-6 px-6 py-10 bg-white shadow-md overflow-hidden sm:rounded-lg"])}}>
+    <div {{$attributes->merge(['class' => "w-full sm:max-w-3xl mt-6 px-6 py-10 bg-white shadow-md overflow-hidden sm:rounded-lg"])}}>
         {{ $slot }}
     </div>
 </div>

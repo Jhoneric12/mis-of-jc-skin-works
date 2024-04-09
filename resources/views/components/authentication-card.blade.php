e46c17aff3de7da10a70a961096de77aa13b2cb3
unknown <jhonerix23@gmail.com>
2024-04-09 22:49:04 +0800

    modifid the screen width

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,5 +1,5 @@
-<div class="min-h-screen min-w-full flex flex-col justify-center items-center lg:flex lg:flex-row lg:justify-around pt-6 py-10 sm:pt-0 ">
-    <div class="md:w-[24rem] h-[20rem]">
+<div class="min-h-screen w-full flex flex-col justify-center items-center lg:flex lg:flex-row lg:justify-around pt-6 py-10 sm:pt-0 ">
+    <div>
         {{ $logo }}
     </div>
 

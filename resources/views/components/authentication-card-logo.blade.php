1577771dd1223b2fcd735b076780b87975209d0b
unknown <jhonerix23@gmail.com>
2024-04-10 00:03:42 +0800

    modified size unit in login and signup

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
-<a href="/">
+<a href="/" class="w-full">
     <h1 class="hidden">Welcome to</h1>
-    <img src="{{asset('assets/Essentials/jcslogo.png')}}" alt="" class="w-[3rem] h-[3rem] md:w-[24rem] md:h-[20rem]">
+    <img src="{{asset('assets/Essentials/jcslogo.png')}}" alt="" class="w-16 h-16 md:w-80 md:h-60">
 </a>

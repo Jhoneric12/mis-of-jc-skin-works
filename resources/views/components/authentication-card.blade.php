2f048cdc88a0388947531bc1a987c5ce81945a0e
unknown <jhonerix23@gmail.com>
2024-04-09 23:03:10 +0800

    removed w-full in authentication card

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -3,7 +3,7 @@
         {{ $logo }}
     </div>
 
-    <div {{$attributes->merge(['class' => "w-full sm:max-w-3xl mt-6 px-6 py-10 bg-white shadow-md overflow-hidden sm:rounded-lg"])}}>
+    <div {{$attributes->merge(['class' => "sm:max-w-3xl mt-6 px-6 py-10 bg-white shadow-md overflow-hidden sm:rounded-lg"])}}>
         {{ $slot }}
     </div>
 </div>

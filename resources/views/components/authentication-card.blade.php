71aa3f29e5803b4409f38cfe5182ce02ece32e34
unknown <jhonerix23@gmail.com>
2024-04-09 22:44:20 +0800

    modified the flex in login

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen min-w-full flex flex-col justify-center items-center md:flex md:flex-row md:justify-around pt-6 py-10 sm:pt-0 ">
+<div class="min-h-screen min-w-full flex flex-col justify-center items-center lg:flex lg:flex-row lg:justify-around pt-6 py-10 sm:pt-0 ">
     <div class="md:w-[24rem] h-[20rem]">
         {{ $logo }}
     </div>

41cf8af9a01fab858d0925f95f5535d73249a71f
unknown <jhonerix23@gmail.com>
2024-04-10 23:17:49 +0800

    modified notification contend in admin

--- app/Livewire/Notification/Clinis.php
+++ app/Livewire/Notification/Clinis.php
@@ -3,12 +3,13 @@
 namespace App\Livewire\Notification;
 
 use App\Models\ClinicNotif;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Clinis extends Component
 {
     public function render()
     {
-        return view('livewire.notification.clinis', ['notifications' => ClinicNotif::where('user_id', ['admin', 'staff', 'patient'])->orderBy('created_at', 'desc')->get()]);
+        return view('livewire.notification.clinis', ['notifications' => ClinicNotif::orderBy('created_at', 'desc')->get()]);
     }
 }

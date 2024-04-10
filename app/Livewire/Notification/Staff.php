8494d58d38cc5f74a0a8553d220741b1b7b6ee20
unknown <jhonerix23@gmail.com>
2024-04-10 23:22:28 +0800

    modified notification contend in staff

--- app/Livewire/Notification/Staff.php
+++ app/Livewire/Notification/Staff.php
@@ -10,6 +10,6 @@ class Staff extends Component
 {
     public function render()
     {
-        return view('livewire.notification.staff', ['notifications' => ClinicNotif::whereIn('type', ['patient', 'staff'])->orderBy('created_at', 'desc')->get()]);
+        return view('livewire.notification.staff', ['notifications' => ClinicNotif::orderBy('created_at', 'desc')->get()]);
     }
 }

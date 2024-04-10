69fc3b9ab31da7aa0925c77a960ca6e628d8be67
unknown <jhonerix23@gmail.com>
2024-04-10 23:05:15 +0800

    modified notifications in admin

--- app/Livewire/Notification/Clinis.php
+++ app/Livewire/Notification/Clinis.php
@@ -3,13 +3,12 @@
 namespace App\Livewire\Notification;
 
 use App\Models\ClinicNotif;
-use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Clinis extends Component
 {
     public function render()
     {
-        return view('livewire.notification.clinis', ['notifications' => ClinicNotif::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get()]);
+        return view('livewire.notification.clinis', ['notifications' => ClinicNotif::where('user_id', ['admin', 'staff', 'patient'])->orderBy('created_at', 'desc')->get()]);
     }
 }
